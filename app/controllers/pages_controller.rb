9deeec11d6b597135acdd152ffaaa81779afb0bc
deeeki <deeeki@gmail.com>
2014-04-27 01:38:31 +0900

    Render README as index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    content = File.read(Rails.root.join('README.md'))
+    render text: GitHub::Markdown.render_gfm(content), layout: true
   end
 end
