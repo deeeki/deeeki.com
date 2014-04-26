2c8fa6416ef6e1cd4ee4065f4bd742cb4ef0066a
deeeki <deeeki@gmail.com>
2014-04-27 01:11:03 +0900

    Purge

--- app/models/work.rb
+++
@@ -1,7 +0,0 @@
-class Work < ActiveRecord::Base
-  attr_accessible :description, :name, :url
-
-  def capture
-    "http://capture.heartrails.com/?#{url}" if url
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Website::Application.config.secret_token = 'a285f027ad27f254fb53c5821414f3347757b182653319ce9d642937cf685806e42ea8677ea10f7f9a078287d6e952ef8baa7869b1461518910b6a84fb680b64'
