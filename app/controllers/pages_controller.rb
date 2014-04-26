0a5333d014cc0ce74b3b88a3bb942e78846e9b8b
deeeki <deeeki@gmail.com>
2014-04-27 03:37:11 +0900

    Enable page caching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  caches_page :index
+
   def index
     content = File.read(Rails.root.join('README.md'))
     render text: GitHub::Markdown.render_gfm(content), layout: true
